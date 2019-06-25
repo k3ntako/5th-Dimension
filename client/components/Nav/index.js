8a9914a86358257c4967e56ffe1ddd39ad0b7045
k3ntako <kentarokaneki@gmail.com>
2019-06-25 01:23:50 -0400

    Mobile friendly nav buttons

--- client/components/Nav/index.js
+++ client/components/Nav/index.js
@@ -4,11 +4,14 @@ import BookSearchBar from './../BookSearchBar';
 import { FaMoon } from "react-icons/fa";
 import { FaSun } from "react-icons/fa";
 import { MdInfo } from "react-icons/md";
+import { FaBars } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 
 import styles from './index.css';
 
 export default (props) => {
   const [ isDarkModeOn, setIsDarkModeOn ] = useState(false);
+  const [ menuActive, setMenuActive ] = useState(false);
 
   // Acts as componentDidMount
   useEffect(() => {
@@ -24,7 +27,14 @@ export default (props) => {
     setIsDarkModeOn(!isDarkModeOn);
   }
 
+  const menuToggleDarkMode = () => {
+    setMenuActive(false);
+    toggleDarkMode();
+  }
+
   const darkModeIcon = isDarkModeOn ? <FaMoon size="1.8rem"/> : <FaSun size="1.8rem"/>;
+  const menuIcon = menuActive ? <FaTimes size="2rem" onClick={() => setMenuActive(false)}/> :
+    <FaBars size="2rem" onClick={() => setMenuActive(true)}/>;
 
   return <div className={styles.navWrapper}>
     <div className={styles.nav}>
@@ -35,7 +45,15 @@ export default (props) => {
         </Link>
         <BookSearchBar />
       </div>
-      <div className={styles.buttons}>
+      <div className={`${styles.menu} ${styles.mobile}`}>
+        { menuIcon }
+        <div className={`${styles.menuItems} ${menuActive ? styles.showMenu : ""}`}>
+          <div className={styles.backgroundClick} onClick={() => setMenuActive(false)}/>
+          <Link to="/about" onClick={() => setMenuActive(false)}>About 5th Dimension </Link>
+          <a onClick={menuToggleDarkMode}>Turn {isDarkModeOn ? "Off" : "On"} Dark Mode</a>
+        </div>
+      </div>
+      <div className={`${styles.buttons} ${styles.desktop}`}>
         <div className={styles.settings} onClick={toggleDarkMode}>
           { darkModeIcon }
         </div>
