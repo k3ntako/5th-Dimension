a7aecb68d25e7f60e3511a98136c6a7a3886cf92
k3ntako <kentarokaneki@gmail.com>
2019-07-06 20:39:38 -0400

    Nav bar rewrite as class component

--- client/components/Nav/index.js
+++ client/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import BookSearchBar from './../BookSearchBar';
 import { FaMoon } from "react-icons/fa";
@@ -9,58 +9,79 @@ import { FaTimes } from "react-icons/fa";
 
 import styles from './index.css';
 
-export default (props) => {
-  const [ isDarkModeOn, setIsDarkModeOn ] = useState(false);
-  const [ menuActive, setMenuActive ] = useState(false);
+export default class Nav extends Component{
+  constructor(props){
+    super(props);
 
-  // Acts as componentDidMount
-  useEffect(() => {
+    this.state = {
+      isDarkModeOn: false,
+      isMenuActive: false,
+    };
+  }
+
+  componentDidMount(){
     const darkModeStatus = localStorage.getItem('dark-mode');
-    setIsDarkModeOn(darkModeStatus === "on");
-    document.getElementsByTagName("body")[0].setAttribute("dark-mode", darkModeStatus);
-  }, []);
+    this.setState({
+      isDarkModeOn: darkModeStatus === "on"
+    });
 
-  const toggleDarkMode = () => {
-    const darkModeStatus = isDarkModeOn ? "off" : "on";
+    document.getElementsByTagName("body")[0].setAttribute("dark-mode", darkModeStatus);
+  }
+
+  toggleDarkMode = () => {
+    const darkModeStatus = this.state.isDarkModeOn ? "off" : "on";
     document.getElementsByTagName("body")[0].setAttribute("dark-mode", darkModeStatus);
     localStorage.setItem('dark-mode', darkModeStatus);
-    setIsDarkModeOn(!isDarkModeOn);
+    this.setState( state => {
+      return {
+        isDarkModeOn: !state.isDarkModeOn
+      }
+    });
   }
 
-  const menuToggleDarkMode = () => {
-    setMenuActive(false);
-    toggleDarkMode();
+  menuToggleDarkMode = () => {
+    this.setState({
+      isMenuActive: false
+    }, this.toggleDarkMode);
   }
 
-  const darkModeIcon = isDarkModeOn ? <FaMoon size="1.8rem"/> : <FaSun size="1.8rem"/>;
-  const menuIcon = menuActive ? <FaTimes size="2rem" onClick={() => setMenuActive(false)}/> :
-    <FaBars size="2rem" onClick={() => setMenuActive(true)}/>;
+  setIsMenuActive( isMenuActive ){
+    this.setState({ isMenuActive });
+  }
 
-  return <div className={styles.navWrapper}>
-    <div className={styles.nav}>
-      <div className={styles.titleAndSearch}>
-        <Link to="/search">
-          <h3 className={`websiteName ${styles.mobile}`}>5D</h3>
-          <h3 className={`websiteName ${styles.desktop}`}>5th Dimension</h3>
-        </Link>
-        <BookSearchBar />
-      </div>
-      <div className={`${styles.menu} ${styles.mobile}`}>
-        { menuIcon }
-        <div className={`${styles.menuItems} ${menuActive ? styles.showMenu : ""}`}>
-          <div className={styles.backgroundClick} onClick={() => setMenuActive(false)}/>
-          <Link to="/about" onClick={() => setMenuActive(false)}>About 5th Dimension</Link>
-          <a onClick={menuToggleDarkMode}>Turn {isDarkModeOn ? "Off" : "On"} Dark Mode</a>
+  render(){
+    const { isDarkModeOn, isMenuActive } = this.state;
+
+    const darkModeIcon = isDarkModeOn ? <FaMoon size="1.8rem"/> : <FaSun size="1.8rem"/>;
+  const menuIcon = isMenuActive ? <FaTimes size="2rem" onClick={() => this.setIsMenuActive(false)}/> :
+      <FaBars size="2rem" onClick={() => this.setIsMenuActive(true)}/>;
+
+    return <div className={styles.navWrapper}>
+      <div className={styles.nav}>
+        <div className={styles.titleAndSearch}>
+          <Link to="/search">
+            <h3 className={`websiteName ${styles.mobile}`}>5D</h3>
+            <h3 className={`websiteName ${styles.desktop}`}>5th Dimension</h3>
+          </Link>
+          <BookSearchBar />
         </div>
-      </div>
-      <div className={`${styles.navButtons} ${styles.desktop}`}>
-        <div className={styles.settings} onClick={toggleDarkMode}>
-          { darkModeIcon }
+        <div className={`${styles.menu} ${styles.mobile}`}>
+          { menuIcon }
+          <div className={`${styles.menuItems} ${isMenuActive ? styles.showMenu : ""}`}>
+            <div className={styles.backgroundClick} onClick={() => this.setIsMenuActive(false)}/>
+            <Link to="/about" onClick={() => this.setIsMenuActive(false)}>About 5th Dimension</Link>
+            <a onClick={this.menuToggleDarkMode}>Turn {isDarkModeOn ? "Off" : "On"} Dark Mode</a>
+          </div>
         </div>
-        <div className={styles.info}>
-          <Link to="/about"><MdInfo size="2rem"/></Link>
+        <div className={`${styles.navButtons} ${styles.desktop}`}>
+          <div className={styles.settings} onClick={this.toggleDarkMode}>
+            { darkModeIcon }
+          </div>
+          <div className={styles.info}>
+            <Link to="/about"><MdInfo size="2rem"/></Link>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  }
 }
